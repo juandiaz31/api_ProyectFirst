1b0eabbcc0702f450206a886f402af3844c941de
Juan Camilo <juanfable@gmail.com>
2021-10-16 19:46:56 -0500

    inicio de la conexion con el backend autenticacion

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ import { conectarBD } from "./db/db.js";
 import rutasPrenda from "./views/prendas/rutas.js";
 import rutasUsuario from "./views/usuarios/rutas.js";
 import rutasVenta from "./views/ventas/rutas.js";
+import jwt from "express-jwt";
+import jwks from "jwks-rsa"
+
 
 
 dotenv.config({ path: "./.env" });
@@ -16,6 +19,19 @@ const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://misiontic-ventasprendas.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'Api-autenticacion-ventaPrendas-mintic',
+issuer: 'https://misiontic-ventasprendas.us.auth0.com/',
+algorithms: ['RS256']
+});
+app.use(jwtCheck);
 app.use(rutasPrenda);
 app.use(rutasUsuario);
 app.use(rutasVenta);
